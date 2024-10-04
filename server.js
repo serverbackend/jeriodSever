bf0301388dee3d0ee85a95e06d15c7cff1e7e19f
serverbackend <jeffryan401@gmail.com>
2024-10-04 07:11:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "jeroidnghelp@gmail.com";
 const pass = "qpqdryzmwyqcqlet";
-// 15 OCT
+// 3rd nov
 
 // Middleware
 app.use(express.json());
